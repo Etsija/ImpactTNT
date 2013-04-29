f9c941bcf8c6d71c99aa57de59ffdf1a9a273285
Jyrki Keisala <jyrki.keisala@gmail.com>
2013-04-29 19:45:38 +0300

    Now the entity handling works as intended. Some cleanup as well

--- src/com/github/etsija/impacttnt/ImpactTNTPlayerListener.java
+++ src/com/github/etsija/impacttnt/ImpactTNTPlayerListener.java
@@ -24,6 +24,7 @@ public class ImpactTNTPlayerListener implements Listener {
 	
 	Logger _log = Logger.getLogger("Minecraft"); // Write debug info to console
 	
+	// Handles throwing of the TNTs
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		final Player player = event.getPlayer();
@@ -31,26 +32,33 @@ public class ImpactTNTPlayerListener implements Listener {
 		double speedFactor = 1.5;
 		final Location handLocation = player.getLocation();
 		handLocation.setY(handLocation.getY() + 1);
+		// Safety radius squared (less complicated to calculate when srqt() left out)
 		final double squaredSafetyDistance = ImpactTNT.safetyRadius * ImpactTNT.safetyRadius;
 		
 		Vector direction = handLocation.getDirection();
 		Entity entity = null;
 		
 		boolean perms = player.isOp() || player.hasPermission("impacttnt.throw");
-		if(perms && event.getMaterial() == Material.TNT && event.getAction() == Action.LEFT_CLICK_AIR){
+		if(perms && event.getMaterial() == Material.TNT && event.getAction() == Action.LEFT_CLICK_AIR) {
 			entity = world.spawn(handLocation, TNTPrimed.class);
 			((TNTPrimed) entity).setFuseTicks(ImpactTNT.fuseTicks);
 			
+			// If TNT configured to detonate on impact
 			if (ImpactTNT.expOnImpact) {
 				CommonEntity<Entity> tntEntity = CommonEntity.get(entity);
+				// This is a controller from Bergerkiller's awesome BKCommonLib
 				tntEntity.setController(new EntityController<CommonEntity<TNTPrimed>>() {
 					public void onTick() {
 						super.onTick();
+						// (Squared) distance of the thrown TNT from the thrower
 						double squaredDistance = handLocation.distanceSquared(entity.getLocation());
+						// This is the explosion logic: TNT explodes
+						// 1. If its movement is impaired (ie. it hits a wall or something)
+						// 2. If it's not moving anymore
+						// 3. If it gets to the ground (1. and 2. do not seem to detect coming back to the ground)
 						if (entity.isMovementImpaired() || !entity.isMoving() || entity.isOnGround()) {
 							if (squaredDistance > squaredSafetyDistance) {
-								entity.remove();
-								world.createExplosion(entity.getLocation(), ImpactTNT.expRadius);
+								entity.getEntity().setFuseTicks(0);
 								return;
 							} else {
 								//player.sendMessage(ChatColor.GREEN + "ImpactTNT: safety fuse applied, immediate detonation cancelled");
@@ -61,6 +69,7 @@ public class ImpactTNTPlayerListener implements Listener {
 				});
 			}
 			
+			// Throw the TNT, using the speedFactor as a modifier
 			entity.setVelocity(direction.multiply(speedFactor));
 			if (player.getGameMode() != GameMode.CREATIVE){
 				player.getInventory().removeItem(new ItemStack(Material.TNT , 1 ));
